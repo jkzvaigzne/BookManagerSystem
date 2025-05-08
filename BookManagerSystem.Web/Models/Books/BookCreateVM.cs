3268f75f194c4b543dcc8c86f515883682e7476d
Jēkabs Andrejs Zvaigzne <47506475+juk-developer@users.noreply.github.com>
2025-05-08 16:24:56 +0300

    Adding Validation Rule

--- BookManagerSystem.Web/Models/Books/BookCreateVM.cs
+++ BookManagerSystem.Web/Models/Books/BookCreateVM.cs
@@ -5,8 +5,10 @@ namespace BookManagerSystem.Web.Models.Books
     public class BookCreateVM
     {
         [Required]
+        [Length(4, 150, ErrorMessage = "Title must be between 4 and 150 characters long.")]
         public string Title { get; set; }
         [Required]
+        [Length(4, 150, ErrorMessage = "Author must be between 4 and 150 characters long.")]
         public string Author { get; set; }
         public DateTime? PublishedDate { get; set; }
     }
