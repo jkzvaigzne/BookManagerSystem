76e044b0897f43a084890935bb6efaf07239ff61
Jēkabs Andrejs Zvaigzne <47506475+juk-developer@users.noreply.github.com>
2025-05-09 10:44:32 +0300

    Probably Need It In Future

--- BookManagerSystem.Web/Data/ApplicationDbContext.cs
+++ BookManagerSystem.Web/Data/ApplicationDbContext.cs
@@ -11,5 +11,28 @@ namespace BookManagerSystem.Web.Data
         }
 
         public DbSet<Book> Books { get; set; }
+
+        //protected override void OnModelCreating(ModelBuilder builder)
+        //{
+        //    base.OnModelCreating(builder);
+
+        //    builder.Entity<Book>(entity =>
+        //    {
+        //        entity.HasKey(b => b.Id);
+
+        //        entity.Property(b => b.Title)
+        //              .IsRequired()
+        //              .HasMaxLength(150)
+        //              .HasColumnType("nvarchar(150)");
+
+        //        entity.Property(b => b.Author)
+        //              .IsRequired()
+        //              .HasMaxLength(150)
+        //              .HasColumnType("nvarchar(150)");
+
+        //        entity.Property(b => b.PublishedDate)
+        //              .HasColumnType("datetime");
+        //    });
+        //}
     }
 }
